00ca74e7db3d4c5f5ba6208e182b0af1c47ca20e
Sergio Mota <samota@uppenjamo.edu.mx>
2015-09-23 22:50:17 -0500

    Added visualization

--- men/src/regression.py
+++ men/src/regression.py
@@ -1,19 +1,26 @@
+import matplotlib.pyplot as plt
+import numpy
 import random
 
 
-def sample_from_straight_line(a_0, a_1, n, sigma):
-    x_min = 0.0
-    x_max = 10.0
+def evaluate_straight_line(a_0, a_1, x):
+    y = a_0 + a_1 * x
+    return y
 
-    points = []
-    for index in range(0, n):
+
+def sample_from_straight_line(a_0, a_1, n, sigma, x_min, x_max):
+    points = numpy.empty([n, 2])
+    index = 0
+    while index < n:
         x = random.uniform(x_min, x_max)
-        y = a_0 + a_1 * x
+        y = evaluate_straight_line(a_0, a_1, x)
 
         mu = 0.0
         y_noisy = y + random.gauss(mu, sigma)
 
-        points.append([x, y_noisy])
+        points[index, :] = [x, y_noisy]
+
+        index += 1
 
     return points
 
@@ -49,12 +56,29 @@ def main():
     print 'True parameters:\na_0 = {:0.3f}, a_1 = {:0.3f}'.format(a_0, a_1)
 
     n = 10
-    sigma = 0.1
-    print 'Sampling {:d} points with sigma = {:0.3f} ...'.format(n, sigma)
-    points = sample_from_straight_line(a_0, a_1, n, sigma)
+    sigma = 0.0
+    x_min = 0.0
+    x_max = 10.0
+    print 'Sampling {:d} points with sigma = {:0.3f} in the interval [{:0.3f}, {:0.3f}] ...'.format(n, sigma, x_min, x_max)
+    points = sample_from_straight_line(a_0, a_1, n, sigma, x_min, x_max)
 
     [a_0_hat, a_1_hat] = compute_linear_least_squares_regression(points)
     print 'Computed parameters\na_0 = {:0.3f}, a_1 = {:0.3f}'.format(a_0_hat, a_1_hat)
+
+    # plot data
+    y_min = evaluate_straight_line(a_0, a_1, x_min)
+    y_max = evaluate_straight_line(a_0, a_1, x_max)
+    plt.plot([x_min, x_max], [y_min, y_max], linewidth=7, label='True')
+
+    plt.plot(points[:,0], points[:,1], 'o', markersize=12, label='Observed')
+
+    y_min_hat = evaluate_straight_line(a_0_hat, a_1_hat, x_min)
+    y_max_hat = evaluate_straight_line(a_0_hat, a_1_hat, x_max)
+    plt.plot([x_min, x_max], [y_min_hat, y_max_hat], 'r', linewidth=5, linestyle='--', label='Approximated')
+
+    plt.legend(loc='lower right')
+
+    plt.show()
 
 
 if __name__ == '__main__':
