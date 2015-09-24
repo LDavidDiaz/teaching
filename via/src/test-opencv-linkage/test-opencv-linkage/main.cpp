72a1f63e9669c9815741225d8d849cd3b495c1dc
Sergio Mota <samota@uppenjamo.edu.mx>
2015-09-24 13:50:00 -0500

    Added test for OpenCV linkage

---
+++ via/src/test-opencv-linkage/test-opencv-linkage/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include <opencv2/core/core.hpp>
+
+using namespace ::cv;
+using namespace ::std;
+
+int main() {
+	cout << "OpenCV version: " << CV_VERSION << endl;
+
+	Mat image;
+}
